f859753e570f41719c4d92d371a72232092b4e86
tavvfiq <taufiq.w@mail.ugm.ac.id>
2020-09-02 22:21:53 +0700

    add header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import logo from "../../assets/img/logo.png";
+import search_icon from "../../assets/img/search.png";
+import chat_icon from "../../assets/img/chat.png";
+import bell_icon from "../../assets/img/bell.png";
+import menu_icon from "../../assets/img/menu.webp";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="logo">
+        <img src={logo} alt="" />
+      </div>
+      <div className="search-group">
+        <div className="search-icon">
+          <img src={search_icon} alt="" />
+        </div>
+        <input type="text" name="search" placeholder="Search" />
+      </div>
+      <div className="menu-icon">
+        <img src={menu_icon} alt="" />
+      </div>
+      <button className="home-button">Home</button>
+      <div className="user-group">
+        <div className="user-icon">
+          <p>T</p>
+        </div>
+        <div id="name">
+          <p>Taufiq</p>
+        </div>
+      </div>
+      <div className="icon-group">
+        <div className="box"></div>
+        <div className="chat">
+          <img src={chat_icon} alt="" />
+        </div>
+        <div className="notification">
+          <img src={bell_icon} alt="" />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
