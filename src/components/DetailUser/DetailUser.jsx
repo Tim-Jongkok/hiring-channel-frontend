2124ef1d42ed85c245a772b28511f95994ad189c
ganawa <ghana.afani@gmail.com>
2020-09-03 21:21:32 +0700

    [update] temp state

--- src/components/DetailUser/DetailUser.jsx
+++ src/components/DetailUser/DetailUser.jsx
@@ -7,7 +7,16 @@ import starIcon from '../../assets/img/star.png';
 import checkIcon from '../../assets/img/check.png';
 
 class DetailUser extends Component {
-   state = {}
+   state = {
+      firstName: "Budi",
+      lastName: "Ramadhan",
+      field: "Frontend Developer",
+      rate: 97,
+      project: 47,
+      description: "I worked more than 8 years in Software Industry. Widely recognized as the most comprehensive and rigorous full-stack developer in the country; I work as Virtual Interim CTO and Full Stack Lead developer, seeking for a new opportunities.",
+      skill: "NodeJs, React, React Native, Binomo",
+      image: profilImg,
+   }
    render() {
       return (
          <>
@@ -29,54 +38,57 @@ class DetailUser extends Component {
                         <div className="row no-gutters">
                            <div className="col text-center text-lg-left">
                               <div className="profil-pict">
-                                 <img src={profilImg} alt="..." />
+                                 <img src={this.state.image} alt={`gambar ${this.state.firstName}${this.state.lastName}`} />
                               </div>
-                              <h5>@budiramadhan</h5>
-                              <p><img src={starIcon} alt="" className="small-icon" /> 97% Success Rate</p>
-                              <p><img src={checkIcon} alt="" className="small-icon" /> 47 Project</p>
+                              <h5>@{(`${this.state.firstName}${this.state.lastName}`).toLowerCase()}</h5>
+                              <p><img src={starIcon} alt="" className="small-icon" /> {this.state.rate}% Success Rate</p>
+                              <p><img src={checkIcon} alt="" className="small-icon" /> {this.state.project} Project</p>
                            </div>
                         </div>
                      </div>
                      {/* End of Profile photo */}
 
                      {/* Description */}
-                     <div className="col-12 col-lg-9 mx-lg-4 mt-lg-4">
+                     <div className="col-12 col-lg-9 px-lg-4 mt-lg-4 ">
                         <div className="row no-gutters mb-3 mt-lg-5">
                            <div className="col text-center text-lg-left">
-                              <h2>Budi Ramadhan</h2>
-                              <h5>Frontend Developer</h5>
+                              <h2>{this.state.firstName}&nbsp;{this.state.lastName}</h2>
+                              <h5>{this.state.field}</h5>
                            </div>
                         </div>
                         <div className="row no-gutters mt-lg-5">
                            <div className="col px-3 px-lg-0">
                               <div className="text-justify">
-                                 <p>I worked more than 8 years in Software Industry. Widely recognized as the most comprehensive and rigorous full-stack developer in the country;<br /> <br />
-                                 I work as Virtual Interim CTO and Full Stack Lead developer, seeking for a new opportunities.</p>
-                                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati doloremque aliquam praesentium aspernatur recusandae quaerat optio nisi ipsam eveniet similique, facere quae repellendus, quia officiis? Sint, excepturi? Architecto, nemo consequatur.</p>
+                                 <p>{this.state.description}</p>
                               </div>
                            </div>
                         </div>
-                        <div className="row no-gutters skill-parent">
-                           <div className="col text-center text-lg-left">
-                              <h6>Skills : </h6>
-                              <h6>NodeJs, React, React Native, Binomo</h6>
-                           </div>
-                        </div>
+
                      </div>
                      {/* End of Description */}
 
                   </div>
-                  <div className="row no-gutters ml-lg-5 mb-5 mb-lg-0 mt-3 mt-lg-0 btn-hire-parent">
-                     <div className="col-lg-2 order-lg-first order-2 text-center mb-2">
-                        <button type="button" className="btn btn-hire">Hire Me</button>
+
+                  <div className="content-bottom">
+                     <div className="row no-gutters ml-lg-5 px-lg-4">
+                        <div className="col offset-lg-2 text-center text-lg-left ">
+                           <h6>Skills : </h6>
+                           <h6>{this.state.skill}</h6>
+                        </div>
                      </div>
-                     <div className="col-lg-2 text-center text-lg-left ml-lg-2 mb-2">
-                        <button type="button" className="btn btn-link btn-history"><h6>History</h6></button>
-                     </div>
-                     <div className="col-lg-auto ml-lg-auto text-center order-last mr-lg-5 mb-2">
-                        <button type="button" className="btn btn-warning text-white">Edit</button>
+                     <div className="row no-gutters mb-5 mb-lg-0 mt-3">
+                        <div className="col-lg-2 ml-lg-5 order-lg-first order-2 text-center mb-2">
+                           <button type="button" className="btn btn-hire">Hire Me</button>
+                        </div>
+                        <div className="col-lg-2 text-center text-lg-left mb-2 ">
+                           <button type="button" className="btn btn-link btn-history"><h6>History</h6></button>
+                        </div>
+                        <div className="col-lg-auto ml-lg-auto text-center order-last mr-lg-5 mb-2">
+                           <button type="button" className="btn btn-warning text-white">Edit</button>
+                        </div>
                      </div>
                   </div>
+
                </div>
                {/* End of Body */}
 
